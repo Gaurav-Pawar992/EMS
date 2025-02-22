6608acd4c5f6962e3cdcfddcb58f562d34a6d911
Gaurav-Pawar992 <gauravpawar9921@gmail.com>
2025-02-22 12:40:33 +0530

    Added CrossOrigin For Axios

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.employee.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
+@CrossOrigin("*")         // Added For React Frontend Axios port number issue 
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
